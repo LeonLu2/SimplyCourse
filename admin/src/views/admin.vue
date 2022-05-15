83a4d1acbebabcff13dc414453d72fd470fdaeb0
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-15 18:10:40 -0400

    1. click the sidebar menu can refresh and add activate style

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -353,16 +353,16 @@
         </div><!-- /.sidebar-shortcuts -->
 
         <ul class="nav nav-list">
-          <li class="">
-            <a href="index.html">
+          <li class="" id="welcome-sidebar">
+            <router-link to="/admin/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
-              <span class="menu-text"> Dashboard </span>
-            </a>
+              <span class="menu-text"> Welcome </span>
+            </router-link>
 
             <b class="arrow"></b>
           </li>
 
-          <li class="active open"> //activate the list and present the list content on screen
+          <li class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> System </span>
@@ -385,13 +385,37 @@
               <li class="">
                 <a href="jqgrid.html">
                   <i class="menu-icon fa fa-caret-right"></i>
-                  Permission management
+                  Permission
                 </a>
 
                 <b class="arrow"></b>
               </li>
             </ul>
           </li>
+
+          <li class="active open">
+            <a href="#" class="dropdown-toggle">
+              <i class="menu-icon fa fa-list"></i>
+              <span class="menu-text"> Business </span>
+
+              <b class="arrow fa fa-angle-down"></b>
+            </a>
+
+            <b class="arrow"></b>
+
+            <ul class="submenu">
+              <li class="active" id="business-chapter-sidebar">
+                <router-link to="/admin/business/chapter">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  Chapters
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
+
+            </ul>
+          </li>
+
         </ul><!-- /.nav-list -->
 
         <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
@@ -447,7 +471,6 @@
 </template>
 
 <script>
-
   export default {
     name: "admin",
     mounted: function(){
@@ -458,6 +481,24 @@
     methods:{
       login(){
         this.$router.push("/admin")
+      },
+
+      /**
+       * 菜单激活样式，id是当前点击的菜单的id
+       * @param id
+       */
+      activeSidebar: function (id) {
+        // 兄弟菜单去掉active样式，自身增加active样式
+        $("#" + id).siblings().removeClass("active");
+        $("#" + id).siblings().find("li").removeClass("active");
+        $("#" + id).addClass("active");
+
+        // 如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
+        let parentLi = $("#" + id).parents("li");
+        if (parentLi) {
+          parentLi.siblings().removeClass("open active");
+          parentLi.addClass("open active");
+        }
       }
     }
   }
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -976,3 +976,14 @@
         </tbody>
       </table>
 </template>
+
+<script>
+  export default {
+    name: "chapter",
+    mounted: function(){
+      this.$parent.activeSidebar("business-chapter-sidebar");
+    },
+    methods:{
+    }
+  }
+</script>
