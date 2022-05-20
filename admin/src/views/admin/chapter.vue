8d5e1154ec3e2a4b9a0b0dd8142926c833e4c100
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-20 17:13:03 -0400

    1. add multiple environment configuration for vue, and change the back-end address by reading the environment variable

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -118,7 +118,7 @@
       list(page) {
         let _this = this;
         Loading.show();
-        _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/list',
             {
               page: page,
               size: _this.$refs.pagination.size,
@@ -144,7 +144,7 @@
         }
 
         Loading.show();
-        _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save', _this.chapter).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/save', _this.chapter).then((response)=>{
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
@@ -165,7 +165,7 @@
 
         Confirm.show("Deleting the chapter is non-revertible，still want to delete?", function () {
           Loading.show();
-          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
+          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/chapter/delete/' + id).then((response)=>{
             Loading.hide();
             let resp = response.data;
             if (resp.success) {
