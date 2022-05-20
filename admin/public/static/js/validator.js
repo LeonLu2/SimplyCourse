b5f9f51eab87d1bf66edfb93a9055cd7485b3173
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-20 14:07:49 -0400

    1. add tool.js and validator.js 2. check null for name and ID or length for ID when saving

---
+++ admin/public/static/js/validator.js
@@ -0,0 +1,19 @@
+Validator = {
+  require: function (value, text) {
+    if (Tool.isEmpty(value)) {
+      Toast.warning(text + "cannot be null");
+      return false;
+    } else {
+      return true
+    }
+  },
+
+  length: function (value, text, min, max) {
+    if (!Tool.isLength(value, min, max)) {
+      Toast.warning(text + "length is " + min + "~" + max + " digits");
+      return false;
+    } else {
+      return true
+    }
+  }
+};
