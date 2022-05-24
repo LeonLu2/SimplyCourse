71d293d5cdcf4bd34f7cf66818fe3143d822aa8d
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-24 12:01:20 -0400

    1. add teacher management

--- admin/src/router.js
+++ admin/src/router.js
@@ -7,6 +7,7 @@ import Category from "./views/admin/category.vue"
 import Course from "./views/admin/course.vue"
 import Chapter from "./views/admin/chapter.vue"
 import Section from "./views/admin/section.vue"
+import Teacher from "./views/admin/teacher.vue"
 
 Vue.use(Router);
 
@@ -35,10 +36,10 @@ export default new Router({
             name: "business/category",
             component: Category,
         }, {
-                    path: "business/course",
-                    name: "business/course",
-                    component: Course,
-                }, {
+            path: "business/course",
+            name: "business/course",
+            component: Course,
+        }, {
             path: "business/chapter",
             name: "business/chapter",
             component: Chapter,
@@ -46,6 +47,10 @@ export default new Router({
             path: "business/section",
             name: "business/section",
             component: Section,
+        }, {
+            path: "business/teacher",
+            name: "business/teacher",
+            component: Teacher,
         }]
     }]
 })
