cf4094d7ef760b3f71b381eaab38192951f7fa2c
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-24 16:00:12 -0400

    1. file upload dev: optimize teacher image display

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -4,6 +4,7 @@ import com.course.server.dto.ResponseDto;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,13 @@ public class UploadController {
 
     public static final String BUSINESS_NAME = "文件上传";
 
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
+
+    @Value("${file.path}")
+    private String FILE_PATH;
+
+
     @RequestMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("上传文件开始：{}", file);
@@ -29,13 +37,13 @@ public class UploadController {
         // 保存文件到本地
         String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = "C:/Users/njlsh/IdeaProjects/saved_files/" + key + "-" + fileName;
+        String fullPath = FILE_PATH + "saved_files/" + key + "-" + fileName;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
 
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent("http://127.0.0.1:9000/file/f/saved_files/" + key + "-" + fileName);
+        responseDto.setContent(FILE_DOMAIN + "f/saved_files/" + key + "-" + fileName);
         return responseDto;
     }
 }
