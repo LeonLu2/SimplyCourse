2aa2f56d6bc08816c2a055cacf0fcb93d4b9a413
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-05-29 21:44:16 -0400

    1. User management and login: add logout function, clear front/back end session-storage

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -303,7 +303,7 @@
                 <li class="divider"></li>
 
                 <li>
-                  <a href="#">
+                  <a v-on:click="logout()" href="#">
                     <i class="ace-icon fa fa-power-off"></i>
                     Logout
                   </a>
@@ -561,7 +561,22 @@
           parentLi.siblings().find("li").removeClass("active");
           parentLi.addClass("open active");
         }
-      }
+      },
+
+      logout () {
+        let _this = this;
+        Loading.show();
+        _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/user/logout').then((response)=>{
+          Loading.hide();
+          let resp = response.data;
+          if (resp.success) {
+            Tool.setLoginUser(null);
+            _this.$router.push("/login")
+          } else {
+            Toast.warning(resp.message)
+          }
+        });
+      },
     }
   }
 </script>
