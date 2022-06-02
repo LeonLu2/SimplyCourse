eb744cc21f43767d0f6dfff265fc1a2c9f4f2737
LeonLu2 <78125134+LeonLu2@users.noreply.github.com>
2022-06-02 17:04:02 -0400

    1. member login/register dev: implement login with captcha 2. add h5 local-storage and session-storage

--- server/src/main/java/com/course/server/dto/MemberDto.java
+++ server/src/main/java/com/course/server/dto/MemberDto.java
@@ -1,104 +1,129 @@
 package com.course.server.dto;
 
-import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
 
 public class MemberDto {
 
-    /**
-     * id
-     */
-    private String id;
+  /**
+   * id
+   */
+  private String id;
 
-    /**
-     * 手机号
-     */
-    private String mobile;
+  /**
+   * 手机号
+   */
+  private String mobile;
 
-    /**
-     * 密码
-     */
-    private String password;
+  /**
+   * 密码
+   */
+  private String password;
 
-    /**
-     * 昵称
-     */
-    private String name;
+  /**
+   * 昵称
+   */
+  private String name;
 
-    /**
-     * 头像url
-     */
-    private String photo;
+  /**
+   * 头像url
+   */
+  private String photo;
 
-    /**
-     * 注册时间
-     */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date registerTime;
+  /**
+   * 图片验证码
+   */
+  private String imageCode;
 
-    public String getId() {
-        return id;
-    }
+  /**
+   * 图片验证码token
+   */
+  private String imageCodeToken;
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  /**
+   * 注册时间
+   */
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private Date registerTime;
 
-    public String getMobile() {
-        return mobile;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getMobile() {
+    return mobile;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setMobile(String mobile) {
+    this.mobile = mobile;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public String getPhoto() {
-        return photo;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Date getRegisterTime() {
-        return registerTime;
-    }
+  public String getPhoto() {
+    return photo;
+  }
 
-    public void setRegisterTime(Date registerTime) {
-        this.registerTime = registerTime;
-    }
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
 
+  public Date getRegisterTime() {
+    return registerTime;
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", mobile=").append(mobile);
-        sb.append(", password=").append(password);
-        sb.append(", name=").append(name);
-        sb.append(", photo=").append(photo);
-        sb.append(", registerTime=").append(registerTime);
-        sb.append("]");
-        return sb.toString();
-    }
+  public void setRegisterTime(Date registerTime) {
+    this.registerTime = registerTime;
+  }
+
+  public String getImageCode() {
+    return imageCode;
+  }
+
+  public void setImageCode(String imageCode) {
+    this.imageCode = imageCode;
+  }
+
+  public String getImageCodeToken() {
+    return imageCodeToken;
+  }
+
+  public void setImageCodeToken(String imageCodeToken) {
+    this.imageCodeToken = imageCodeToken;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuffer sb = new StringBuffer("MemberDto{");
+    sb.append("id='").append(id).append('\'');
+    sb.append(", mobile='").append(mobile).append('\'');
+    sb.append(", password='").append(password).append('\'');
+    sb.append(", name='").append(name).append('\'');
+    sb.append(", photo='").append(photo).append('\'');
+    sb.append(", registerTime=").append(registerTime);
+    sb.append(", imageCode='").append(imageCode).append('\'');
+    sb.append(", imageCodeToken='").append(imageCodeToken).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
 
 }
